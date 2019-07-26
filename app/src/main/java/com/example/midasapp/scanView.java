1de11c7ce82ad7b93e84ce2a5c88c4e70d788bab
kukuminer <liranzh2000@gmail.com>
2019-07-26 19:31:33 -0400

    Functioning camera preview on textureView Finally

--- app/src/main/java/com/example/midasapp/scanView.java
+++ app/src/main/java/com/example/midasapp/scanView.java
@@ -1,26 +1,43 @@
 package com.example.midasapp;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Size;
 import android.util.SparseArray;
+import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
+import org.w3c.dom.Text;
+
+import java.util.Collections;
 
 
 public class scanView extends AppCompatActivity {
@@ -29,41 +46,231 @@ public class scanView extends AppCompatActivity {
     private TextureView textureView;
     private Button button;
 
-    CameraManager manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+    final static int CAMERA_REQUEST_CODE = 1;
+
+    CameraManager cameraManager;
+    TextureView.SurfaceTextureListener surfaceTextureListener;
+    int cameraFacing;
+    Size previewSize;
+    String cameraId;
+    CameraDevice.StateCallback stateCallback;
+    Handler backgroundHandler;
+    HandlerThread backgroundThread;
+    private CameraDevice cameraDevice;
+    CameraCaptureSession cameraCaptureSession;
+    CaptureRequest captureRequest;
+    CaptureRequest.Builder captureRequestBuilder;
+
+
+
 
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_view);
 
-
+        textureView = (TextureView)  findViewById(R.id.textureView);
         valueText = (TextView) findViewById(R.id.txtContent);
+        button = (Button) findViewById(R.id.button);
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        cameraFacing = CameraCharacteristics.LENS_FACING_BACK;
+
+        surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+                setUpCamera();
+                openCamera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+            }
+        };
+
+        stateCallback = new CameraDevice.StateCallback() {
+            @Override
+            public void onOpened(CameraDevice cameraDevice) {
+                scanView.this.cameraDevice = cameraDevice;
+                createPreviewSession();
+            }
+
+            @Override
+            public void onDisconnected(CameraDevice cameraDevice) {
+                cameraDevice.close();
+                scanView.this.cameraDevice = null;
+            }
+
+            @Override
+            public void onError(CameraDevice cameraDevice, int error) {
+                cameraDevice.close();
+                scanView.this.cameraDevice = null;
+            }
+        };
+    }
+    private void setUpCamera()
+    {
         try
         {
-            CameraDevice.StateCallback
-
-        }catch(CameraAccessException e)
+            for (String cameraId : cameraManager.getCameraIdList())
+            {
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == cameraFacing)
+                {
+                    StreamConfigurationMap streamConfigurationMap = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    previewSize = streamConfigurationMap.getOutputSizes(SurfaceTexture.class)[0];
+                    this.cameraId = cameraId;
+                }
+            }
+        } catch (CameraAccessException e)
         {
             e.printStackTrace();
         }
-
-
-
-
-
     }
+    private void openCamera()
+    {
+        try
+        {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+            {
+                cameraManager.openCamera(cameraId, stateCallback, backgroundHandler);
+            }
+        } catch (CameraAccessException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    private void openBackgroundThread()
+    {
+        backgroundThread = new HandlerThread("camera_background_thread");
+        backgroundThread.start();
+        backgroundHandler = new Handler(backgroundThread.getLooper());
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        openBackgroundThread();
+        if(textureView.isAvailable())
+        {
+            setUpCamera();
+            openCamera();
+        } else {
+            textureView.setSurfaceTextureListener(surfaceTextureListener);
+        }
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        closeCamera();
+        closeBackgroundThread();
+    }
+
+    private void closeCamera()
+    {
+        if(cameraCaptureSession != null)
+        {
+            cameraCaptureSession.close();
+            cameraCaptureSession = null;
+        }
+
+        if(cameraDevice != null)
+        {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    }
+
+    private void closeBackgroundThread()
+    {
+        if (backgroundHandler != null)
+        {
+            backgroundThread.quitSafely();
+            backgroundThread = null;
+            backgroundHandler = null;
+        }
+    }
+
+    private void createPreviewSession()
+    {
+        try
+        {
+            SurfaceTexture surfaceTexture = textureView.getSurfaceTexture();
+            surfaceTexture.setDefaultBufferSize(previewSize.getWidth(), previewSize.getHeight());
+            Surface previewSurface = new Surface(surfaceTexture);
+            captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            captureRequestBuilder.addTarget(previewSurface);
+
+            cameraDevice.createCaptureSession(Collections.singletonList(previewSurface),
+                    new CameraCaptureSession.StateCallback()
+                    {
+
+                        @Override
+                        public void onConfigured(CameraCaptureSession cameraCaptureSession)
+                        {
+                            if (cameraDevice == null)
+                            {
+                                return;
+                            }
+                            try
+                            {
+                                captureRequest = captureRequestBuilder.build();
+                                scanView.this.cameraCaptureSession = cameraCaptureSession;
+                                scanView.this.cameraCaptureSession.setRepeatingRequest(captureRequest, null, backgroundHandler);
+                            } catch (CameraAccessException e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                        @Override
+                        public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+
+                        }
+                    }, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void lock()
+    {
+        try
+        {
+            cameraCaptureSession.capture(captureRequestBuilder.build(), null, backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 
     public void process(View v)
     {
+        //lock();
 
 
 
-
-        /*// BARCODE DETECTION: REQUIRES ADJUSTMENT TO TEXTUREVIEW
+/*
         ImageView i = (ImageView) findViewById(R.id.textureView);
         Bitmap b = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.v6);
         i.setImageBitmap(b);
@@ -93,7 +300,6 @@ public class scanView extends AppCompatActivity {
         {
             Barcode code = barcodes.valueAt(0);
             valueText.setText(code.rawValue);
-        }
-        */
+        }*/
     }
 }
